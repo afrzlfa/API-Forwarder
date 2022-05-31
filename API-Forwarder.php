c4092bfe1b37bdcc7a3e578e6ec3c0e4edab6a43
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2022-06-01 01:15:10 +0700

    Create API-Forwarder.php

---
+++ API-Forwarder.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace APIForwarder;
+
+class API
+{
+
+    public function __construct()
+    {
+
+        $this->domain = '';
+        $this->ssl = '';
+
+    }
+
+    public function requester($url, $method, $headers, $body)
+    {
+
+        $c = curl_init();
+
+        curl_setopt($c, CURLOPT_URL, $url);
+        curl_setopt($c, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($c, CURLOPT_HTTPHEADER, $headers);
+
+        if (!empty($body)) {
+
+            curl_setopt($c, CURLOPT_POSTFIELDS, $body);
+
+        }
+
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+
+        $response = curl_exec($c);
+        curl_close($c);
+
+        return $response;
+
+    }
+
+    public function forwarder()
+    {
+
+        if (!empty($this->ssl) && $this->ssl == true) {
+
+            $protocol = "https://";
+
+        } else {
+
+            $protocol = "http://";
+
+        }
+
+        $requestMethod = $_SERVER['REQUEST_METHOD'];
+
+        if (!empty($_GET['path'])) {
+
+            $url = $protocol . $this->domain . "/" . $_GET['path'];
+
+        } else {
+
+            $url = $protocol . $this->domain;
+
+        }
+
+        $headers = [];
+
+        foreach(getallheaders() as $key => $value)
+        {
+
+            $headers []= $key . ": " . $value;
+
+        }
+
+        if (!empty(file_get_contents("php://input"))) {
+
+            $body = file_get_contents("php://input");
+
+        } else {
+
+            $body = null;
+
+        }
+
+        return $this->requester($url, $requestMethod, $headers, $body);
+
+    }
+
+}
